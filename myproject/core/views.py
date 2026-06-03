92326a96518d4d3d2add08c6dd552b50dbf1eebf
ARSH <arshrakholiya3@gmail.com>
2026-06-03 18:31:16 +0530

    learnt about html with django

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -8,18 +8,23 @@ def home(request):
         'name': "Harsh",
         'age': 20,
         'language': 'python',
+        'is_logged_in': False,
+        'skills': ['Python', 'Java', 'C'],
     }
     return render(request, 'core/home.html', context)
 
+# about page
 def about(request):
     return render(request, 'core/about.html')
 
-# this 
+# contact page
 def contact(request):
     return render(request, 'core/contact.html')
 
+# user profile page
 def user_profile(request, username):
     return HttpResponse(f"This is about page of {username}")
 
+# user id page
 def user_id(request, user_id):
     return HttpResponse(f"Your user id is {user_id}")
