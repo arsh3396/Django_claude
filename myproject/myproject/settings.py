e3cd3a84ac9401b6d5324e8361afe1eb770571d7
ARSH <arshrakholiya3@gmail.com>
2026-06-03 18:53:10 +0530

    added image to html page

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
             ],
         },
+        
     },
 ]
 
@@ -115,4 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
