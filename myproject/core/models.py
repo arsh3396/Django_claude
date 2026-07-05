946a8da23430516edbb6e39ba7ebf7372475682b
ARSH <arshrakholiya3@gmail.com>
2026-07-05 23:06:03 +0530

    added new model category

--- myproject/core/models.py
+++ myproject/core/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Categories"
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -8,5 +17,12 @@ class Post(models.Model):
     author = models.CharField(max_length=200, default='Unknown')
     is_published = models.BooleanField(default=False)
     
+    category = models.ForeignKey(
+        Category,
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True
+    )
+
     def __str__(self):
         return self.title
