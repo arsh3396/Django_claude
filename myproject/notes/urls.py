6972d75527fc9349a48ed31d5df379c434c4f7c6
ARSH <arshrakholiya3@gmail.com>
2026-07-12 22:21:25 +0530

    learnt about modelform

--- myproject/notes/urls.py
+++ myproject/notes/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.notes_list, name='notes_list'),
     path('<int:note_id>', views.note_detail, name='note_detail'),
+    path('add/', views.add_note, name='add_note'),
+    path('edit/<int:note_id>/', views.edit_note, name="edit_note"),
 ]
--- myproject/notes/views.py
+++ myproject/notes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Note
+from .forms import NoteForm
+from django.contrib import messages
 
 # Create your views here.
 def notes_list(request):
@@ -16,3 +18,39 @@ def note_detail(request, note_id):
     }
     return render(request, 'notes/note_detail.html', context)
     
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Note added successfully!")
+            return redirect('notes_list')
+        
+    else: 
+        form = NoteForm()
+        
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'notes/add_note.html', context)
+
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, intstance=note)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Note updated!")
+            return redirect('note_detail', note_id=note.id)
+    
+    else :
+        form = NoteForm(instance=note)
+        
+    context = {
+        'form': form,
+        'note': note
+    }        
+    return render(request, 'notes/edit_note.html', context)
